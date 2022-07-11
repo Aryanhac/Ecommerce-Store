fe5e03da9c2772c92de1485f1c133ce30792705e
Aryan Gupta <202051038@iiitvadodara.ac.in>
2022-07-11 20:11:15 +0530

    fixed db error

--- Ecommerce-Backend/config/database.js
+++ Ecommerce-Backend/config/database.js
@@ -1,8 +1,8 @@
 const mongoose =require('mongoose');
 
 const Database=()=>{
-    return new Promise((resolve,reject)=>{
-        mongoose.connect(process.env.Database_URL,{
+    return new Promise(async (resolve,reject)=>{
+       await mongoose.connect(process.env.Database_URL,{
             useNewURLParser:true,useUnifiedTopology:true
        }).then((data)=>{
            resolve('Database connnected successfully');
